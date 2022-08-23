d37d0bd5f7ddf073ec47333d754518c14dd11625
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-08-23 14:32:02 +0200

    Separate component: City

---
+++ src/components/City.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+const data = {
+    name: 'Valence', 
+    weather: 'Peu nuageux', 
+    temperature: 20.55,
+    updatedAt: new Date().toLocaleDateString() 
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">Welcome</h1>
+    <h3>{{data.name}}</h3>
+    <ul>
+      <li>{{data.weather}}</li>
+      <li>{{data.temperature}} C°</li>
+      <li>{{data.updatedAt}}</li>
+    </ul>
+  </div>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,17 @@
 <script setup>
+
+import City from "./City.vue";
+
 defineProps({
   msg: {
     type: String,
     required: true,
   },
-  /*
-  data() {
-    return {
-    name: 'Ma ville', // nom de la ville
-    weather: 'Peu nuageux', // descriptif météo 
-    temperature: 20.55, // température en °C
-    updatedAt: new Date() // date de dernière mise à jour
-    }
-  }
-  */
 });
-
-const data = {
-    name: 'Valence', // nom de la ville
-    weather: 'Peu nuageux', // descriptif météo 
-    temperature: 20.55, // température en °C
-    updatedAt: new Date() // date de dernière mise à jour
-}
 </script>
 
-
-
 <template>
-  <div class="greetings">
-    <h1 class="green">Welcome</h1>
-    <h3>{{data.name}}</h3>
-    <ul>
-      <li>{{data.weather}}</li>
-      <li>{{data.temperature}}</li>
-      <li>{{data.updatedAt}}</li>
-    </ul>
-  </div>
+<City></City>
 </template>
 
 <style scoped>
