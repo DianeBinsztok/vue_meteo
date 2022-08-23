54971a57d6b226d70566bfb6306fb7b88ab90a40
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-08-23 14:48:47 +0200

    Component City exports data object

--- src/components/City.vue
+++ src/components/City.vue
@@ -1,21 +1,24 @@
-<script setup>
-
-const data = {
+<script>
+export default {
+    data() {
+    return {
     name: 'Valence', 
     weather: 'Peu nuageux', 
     temperature: 20.55,
     updatedAt: new Date().toLocaleDateString() 
+        }
+} 
 }
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">Welcome</h1>
-    <h3>{{data.name}}</h3>
+    <h3>{{name}}</h3>
     <ul>
-      <li>{{data.weather}}</li>
-      <li>{{data.temperature}} C°</li>
-      <li>{{data.updatedAt}}</li>
+      <li>{{weather}}</li>
+      <li>{{temperature}} C°</li>
+      <li>{{updatedAt}}</li>
     </ul>
   </div>
 </template>
